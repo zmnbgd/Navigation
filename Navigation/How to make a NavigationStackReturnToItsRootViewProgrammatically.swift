2c5fa2cd5c674488236c1fea54a83bde0996c733
Marko Živanović <zivanovic.marko@icloud.com>
2025-05-27 14:52:16 +0200

    PartTwo

--- Navigation/How to make a NavigationStackReturnToItsRootViewProgrammatically.swift
+++ Navigation/How to make a NavigationStackReturnToItsRootViewProgrammatically.swift
@@ -9,12 +9,53 @@ import SwiftUI
 
 @Observable
 class PathStore {
-    var path: [Int] {
+    //var path: [Int] {
+    var path: NavigationPath {
         didSet {
             save()
         }
     }
     private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+    
+//    init() {
+//        if let data = try? Data(contentsOf: savePath) {
+//           if let decoded = try? JSONDecoder.decode([Int].self, from: data) {
+//            path = decoded
+//            return
+//            
+//           }
+//        }
+//        
+//        path = []
+//    }
+    
+    let decoder = JSONDecoder()
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            //if let decode = try? decoder.decode([Int].self, from: data) {
+            if let decode = try? decoder.decode(NavigationPath.CodableRepresentation.self, from: data) {
+                //path = decode
+                path = NavigationPath(decode)
+                return
+            }
+        }
+        //path = []
+        path = NavigationPath()
+    }
+    
+    func save() {
+        
+        guard let representation = path.codable else { return }
+        
+        do {
+            //let data = try JSONEncoder().encode(path)
+            let data = try JSONEncoder().encode(representation)
+            try data.write(to: savePath)
+        } catch {
+            print("failed to save navigation data")
+        }
+    }
+    
 }
 
 struct DetailView1: View {
@@ -37,13 +78,16 @@ struct DetailView1: View {
 struct How_to_make_a_NavigationStackReturnToItsRootViewProgrammatically: View {
     
 //    @State private var path = [Int]()
-    @State private var path = NavigationPath()
-    
+    //@State private var path = NavigationPath()
+    @State private var pathStore = PathStore()
     var body: some View {
-        NavigationStack(path: $path) {
-            DetailView1(number: 0, path: $path)
+        //NavigationStack(path: $path) {
+        NavigationStack(path: $pathStore.path) {
+            //DetailView1(number: 0, path: $path)
+            DetailView1(number: 0, path: $pathStore.path)
                 .navigationDestination(for: Int.self) { i in
-                    DetailView1(number: i, path: $path)
+                    //DetailView1(number: i, path: $path)
+                    DetailView1(number: i, path: $pathStore.path)
                 }
         }
     }
